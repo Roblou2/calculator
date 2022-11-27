c957ca471e72b064655ab81998cb374ff8962174
Rob Hamburg <robhamburg89@gmail.com>
2022-11-27 13:47:54 +0000

    get plus operator displaying

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 const display = document.querySelector('.display')
+
 let a = 0; //first number before an operator is called
 
 
@@ -20,28 +21,44 @@ function multi (a, b) {
 return a*b
 }
 
-function operate (a, b) {
+//get operator buttons
+let sum = document.querySelector('.add')
+let prod = document.querySelector('.multi')
+let diff = document.querySelector('.sub')
+let divide = document.querySelector('.divide')
+let equals = document.querySelector('.equals')
 
+//set up code for operator buttons on click 
+let numArr = []
+
+let operator = ""
+
+sum.onclick = () => {
+  let conv = parseInt(numberOne)
+    numArr.push(conv)
+    let operator = "+"     //the switch to use in operate function
+    display.innerHTML = `${numArr[0]} ${operator}`
+ console.log(numArr)
 }
+
+
+//call this function when '=' pressed
+
+
+function operate (a, b, sub, add) {
+diff.addEventListener('click', sub(a, b))
+sum.addEventListener('click', add (a, b))
+}
+
+
 let numberOne = "" //the first number to be operarted on
+
+let numberTwo = "" //second number
+
+
 
 let num = document.querySelectorAll('.num')
 
-function makeNumStr (val) { 
-if (a == 0) {
-    a = val
-    let numStr = a.toString()
-    numberOne = numStr
-    display.innerHTML = numStr
-}
-else if (val > 0) {                   //can still make decimal numbers as strings and then convert to number
-    let next = val
-    let nextStr = next.toString() 
-numberOne += nextStr
-    display.innerHTML = numberOne
-}
-console.log(numberOne)
-}
 
 function loop () {
     for (let i = 0; i < num.length; i++) {
@@ -53,4 +70,19 @@ function loop () {
 window.onload = () => {
     loop ()
 }
+
+function makeNumStr (val) { 
+           //can still make decimal numbers as strings and then convert to number
+  let  next = val
+let nextStr = next.toString() 
+numberOne += nextStr
+    display.innerHTML = numberOne
+       
+    }
+
+
+
+
+//look at making operators into objects//
+
 
